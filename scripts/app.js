13dc0b245b9d0078c45cd3b3d8716a9e1641a595
Matt Tucker <arcanemahuloq@gmail.com>
2023-09-20 20:20:25 -0500

    finished  basic layout and get elements

--- scripts/app.js
+++ scripts/app.js
@@ -5,23 +5,38 @@ function imported() {
 imported();
 
 const cnt = document.querySelector(".container");
-
+const toDoForm = document.querySelector(".toDoForm");
 const enterButton = document.querySelector(".entr");
 
 const deleteButton = document.querySelector(".dlt");
 
-const toDoInput = document.querySelector(".entry");
+const toDoInput = document.querySelector("input[type=text]");
+// const bookmarkInput = bookmarkForm.querySelector("input[type=text]");
 
 const toDoList = document.querySelector(".output");
 
-function testFunction() {
+function testFunction(e) {
+  e.preventDefault(e);
   console.log("Button Worked");
-  let li = document.createElement("li");
 
-  li.appendChild(document.createTextNode("test"));
-  toDoList.appendChild(li);
+  // const title = bookmarkInput.value;
+  // const bookmark = document.createElement("a");
+  // bookmark.className = "bookmark";
+  // bookmark.innerHTML = title;
+  // bookmark.href = "#";
+  // bookmark.target = "blank";
+  // bookmarksList.appendChild(bookmark);
+
+  let addLi = document.createElement("li");
+
+  addLi.appendChild(document.createTextNode(toDoInput.value));
+  toDoList.appendChild(addLi);
+
+  toDoForm.reset();
 }
 
 deleteButton.addEventListener("click", testFunction);
 
 enterButton.addEventListener("click", testFunction);
+
+toDoInput.addEventListener("submit", testFunction);
